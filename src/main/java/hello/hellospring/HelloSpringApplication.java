4bf45cf5fc00e82f10cb3ba9d1978cad4308564a
Lee-DongSeop <76763417+Lee-DongSeop@users.noreply.github.com>
2022-01-15 21:36:39 +0900

    library sequence

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
+	//main method 실행 -> index.html(Welcome page) -> HelloController.java -> hello.html
 	public static void main(String[] args) {
 
 		SpringApplication.run(HelloSpringApplication.class, args);
