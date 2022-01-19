ef22972bf8374906b6d2c43e57d750150a2d43d6
Lee-DongSeop <76763417+Lee-DongSeop@users.noreply.github.com>
2022-01-20 00:26:12 +0900

    git Test

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //***********************SERVER***********************
+//git Test123
 @SpringBootApplication
 public class HelloSpringApplication {
 
