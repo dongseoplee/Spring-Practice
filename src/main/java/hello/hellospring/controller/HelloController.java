cdb959ac844251e7142c6cba761045263f0066ea
Lee-DongSeop <76763417+Lee-DongSeop@users.noreply.github.com>
2022-01-21 20:22:02 +0900

    add hello-template.html and hello-mvc

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,8 +1,10 @@
 package hello.hellospring.controller;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,5 +16,11 @@ public class HelloController {
 
         model.addAttribute("data", "hello 9자Dragon!! (attributeValue 값!)");
         return "hello"; //return hello의 의미는 resources/templates의 hello.html로 가서 렌더링해라 윗줄의 model을 같이 hello.html로 넘겨줌
+    }
+
+    @GetMapping("hello-mvc") //http://localhost:8080/hello-mvc 여기로 Mapping
+    public String helloMvc(@RequestParam("name") String name, Model model) { //http://localhost:8080/hello-mvc?name=spring!
+        model.addAttribute("name", name);
+        return "hello-template"; //return hello-template의 의미는 resources/templates의 hello-template.html로 가서 렌더링해라 윗줄의 model을 같이 hello.html로 넘겨줌
     }
 }
